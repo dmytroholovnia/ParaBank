2966e41dd854a9dfd499dfd08e81f6c758714628
Dmytro Holovnia <dmytro@doubledmarketing.com>
2022-06-14 20:11:08 +0300

    fix double window open, fix driver quit

--- src/test/java/AuthorizationTest.java
+++ src/test/java/AuthorizationTest.java
@@ -3,18 +3,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import static Core.Driver.getDriver;
 
 public class AuthorizationTest {
-
-    private static WebDriver driver;
 
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        getDriver();
     }
 
     @Test
@@ -29,6 +26,8 @@ public class AuthorizationTest {
 
     @After
     public void tearDown() {
-        driver.quit();
+        if (getDriver() != null) {
+            getDriver().quit();
+        }
     }
 }
