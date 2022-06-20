6b6b0aef8cee0ce6e2cd1ffed363289fbc51a6da
Dmytro Holovnia <dmytro@doubledmarketing.com>
2022-06-20 11:31:11 +0300

    changed website

--- src/main/java/Pages/MainPage.java
+++ src/main/java/Pages/MainPage.java
@@ -1,12 +1,23 @@
 package Pages;
 
-import Core.Driver;
+import lombok.Getter;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import static Core.Driver.getDriver;
+
+@Getter
 
 public class MainPage {
 
     public MainPage() {
-        PageFactory.initElements(Driver.getDriver(), this);
+        PageFactory.initElements(getDriver(), this);
     }
 
+    @FindBy(xpath = "//div[@ng-if='showOverview']//h1")
+    private WebElement pageTitle;
+
+    @FindBy(id = "accountTable")
+    private WebElement accountTable;
 }
