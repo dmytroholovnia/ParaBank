3858f4528e1962edacc4a5bc89a8bc22474fc767
Dmytro Holovnia <dmytro@doubledmarketing.com>
2022-07-24 19:33:39 +0300

    uncomment and ignored test, refactoring

---
+++ src/test/java/ui/AuthorizationTest.java
@@ -0,0 +1,55 @@
+package ui;
+
+import core.BaseTest;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Ignore;
+import pages.LoginPage;
+import pages.MainPage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AuthorizationTest extends BaseTest {
+
+    @Test
+    public void authorization() {
+        LoginPage loginPage = new LoginPage(driver);
+
+        System.out.println("Open login page");
+        loginPage.open(driver);
+
+        MainPage mainPage = loginPage.authorize(login, password);
+
+        assertEquals("Title is incorrect",
+                     "Accounts Overview",
+                             mainPage.getPageTitle().getText());
+
+        assertTrue("Accounts table is not present!",
+                             mainPage.getAccountTable().isDisplayed());
+    }
+
+    @Test
+    @Ignore
+    public void authorizationFailed() {
+        LoginPage loginPage = new LoginPage(driver);
+
+        final String expectedMessage = "The username and password could not be verified.";
+        login = RandomStringUtils.randomAlphabetic(10);
+        password = RandomStringUtils.randomAlphabetic(10);
+
+        System.out.println("Open login page");
+        loginPage.open(driver);
+
+        loginPage.authorize(login, password);
+
+        assertEquals("Error title is incorrect",
+                     "Error!",
+                     loginPage.getErrorTitle().getText());
+
+        assertEquals("Eror message is incorrect!",
+                             expectedMessage,
+                             loginPage.getErrorMessage().getText());
+    }
+
+}
