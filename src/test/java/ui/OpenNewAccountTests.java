70baeb7f158dd83f6978639ea8f5f7fc8f9a4818
Dmytro Holovnia <dmytro@doubledmarketing.com>
2022-07-25 21:46:24 +0300

    added names for parameters

--- src/test/java/ui/OpenNewAccountTests.java
+++ src/test/java/ui/OpenNewAccountTests.java
@@ -2,16 +2,18 @@ package ui;
 
 import core.BaseTest;
 import enums.Types;
-import pages.LoginPage;
-import pages.MainPage;
-import pages.OpenNewAccountPage;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.OpenNewAccountPage;
 
-import static org.junit.Assert.*;
-import static org.junit.runners.Parameterized.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.runners.Parameterized.Parameter;
+import static org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class OpenNewAccountTests extends BaseTest {
@@ -19,13 +21,14 @@ public class OpenNewAccountTests extends BaseTest {
     private final LoginPage loginPage = new LoginPage(driver);
     private MainPage mainPage;
 
-    @Parameters
+    @Parameters(name = "{0}")
     public static Object[] accounts() {
         return new Object[]{
                 Types.SAVINGS,
                 Types.CHECKING
         };
     }
+
     @Parameter()
     public Types type;
 
