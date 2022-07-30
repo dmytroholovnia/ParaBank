8a48e58fca297e7a87b34086f0aefbde9bd29e34
Dmytro Holovnia <dmytro@doubledmarketing.com>
2022-07-30 13:48:09 +0300

    fix waiter for selector

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -1,5 +1,8 @@
 package helper;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -31,4 +34,46 @@ public class Helper {
 //
 //        return elements.size() != 0;
 //    }
+
+    public static By getByFromElement(WebElement element) {
+        By by;
+        //[[ChromeDriver: chrome on XP (d85e7e220b2ec51b7faf42210816285e)] -> xpath: //input[@title='Search']]
+        String[] pathVariables = (element
+                .toString()
+                .split("->")[1].replaceFirst("(?s)(.*)]", "$1" + ""))
+                .split(":");
+
+        String selector = pathVariables[0].trim();
+        String value = pathVariables[1].trim();
+
+        switch (selector) {
+            case "id":
+                by = By.id(value);
+                break;
+            case "className":
+                by = By.className(value);
+                break;
+            case "tagName":
+                by = By.tagName(value);
+                break;
+            case "xpath":
+                by = By.xpath(value);
+                break;
+            case "cssSelector":
+                by = By.cssSelector(value);
+                break;
+            case "linkText":
+                by = By.linkText(value);
+                break;
+            case "name":
+                by = By.name(value);
+                break;
+            case "partialLinkText":
+                by = By.partialLinkText(value);
+                break;
+            default:
+                throw new IllegalStateException("locator : " + selector + " not found!");
+        }
+        return by;
+    }
 }
